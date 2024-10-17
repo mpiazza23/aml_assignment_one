dbaabba4d01c7e8ba632184c769da5d51ec869a3
mpiazza23 <83581310+mpiazza23@users.noreply.github.com>
2024-10-17 15:37:39 +0200

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+st.title("AML Assignment 1")
+
+st.write('''Welcome to our university's student submission web app!
+
+To upload your assignments, please proceed to the "Load Results" page and carefully follow the instructions provided there. You may submit as many files as you like; there are no restrictions on the number of uploads.
+
+If you wish to view the leaderboard, visit the "Leaderboard" page. The leaderboard is ordered based on two criteria: the reference metric and the submission time, with priority given to the earliest submissions.
+
+Please note that these submissions are for practice and ranking purposes only and will not impact your final grade in any way.
+
+We hope you find this tool useful for tracking your progress and improving your performance.
+
+For any questions or clarifications, please contact: m.piazza23@campus.unimib.it''')
